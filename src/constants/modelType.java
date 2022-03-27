31d50904012470a050f8c4824f16b61df0b6177a
anthonylopes54 <anthonylopes54@gmail.com>
2022-03-27 12:50:31 -0700

    Adding enums

---
+++ src/constants/modelType.java
@@ -0,0 +1,19 @@
+package constants;
+
+public enum modelType {
+    DRIVES,
+    EMPLOYEE,
+    MANAGES,
+    MAINTAINS,
+    PASSENGER,
+    TRAIN_EXTRA,
+    TRAIN_MAIN_INFO,
+    CARGO_BELONGS_TO,
+    GOES_THROUGH,
+    ROUTE,
+    SEAT_CAR_MAPPING,
+    SEAT_MAIN,
+    STATION,
+    STORED_AT,
+    TRIP
+}
