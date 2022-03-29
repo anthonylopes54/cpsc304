ce65b9aa3e1b205e8bd4322c0e6c2e1a7668f9d0
anthonylopes54 <anthonylopes54@gmail.com>
2022-03-28 17:44:19 -0700

    Completed the join function

--- src/database/Train_MainHandler.java
+++ src/database/Train_MainHandler.java
@@ -4,6 +4,7 @@ import models.*;
 import util.Constants;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
